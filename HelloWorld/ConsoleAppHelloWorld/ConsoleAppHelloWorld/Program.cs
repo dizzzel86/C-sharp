6027fdff96b053541866a4b0f70c604c2974bbcc
dizzzel86 <d.chentsov@my.com>
2023-03-08 13:17:30 +0300

    ver1

---
+++ HelloWorld/ConsoleAppHelloWorld/ConsoleAppHelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHelloWorld
+/// <summary>
+/// Главная составляющая единица приложения - класс
+/// </summary>
+	class Program
+	{
+	/// <summary>
+	/// Код, который выполняется в первую очередь 
+	/// </summary>
+	/// <param name="args"> Параметры запуска</param>
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!"); //Вывод текста
+											   //в консоль
+
+			Console.ReadKey();                 //Ожидание от пользователя 
+											   //нажатия какой-либо кнопки
+
+	}
+	}
+}
